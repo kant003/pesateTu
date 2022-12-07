59fab311254edee50e53b15ace3af65adfc32c56
kant003 <gonzalezm.angel@gmail.com>
2022-12-07 14:56:25 +0100

    solution

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 import './style.css'
+
+document.querySelector('#weight').addEventListener('input', recalculate)
+document.querySelector('#height').addEventListener('change', recalculate)
+
+function recalculate(){
+    const height = document.querySelector('#height').value
+    const weight = document.querySelector('#weight').value
+    document.querySelector('#weightValue').innerHTML = weight 
+    const imc = calculateImc(height,weight)
+    const theClass = calculateClass(imc)
+    document.querySelector('#result').innerHTML=imc.toFixed(2) + ' ' + theClass
+}
+
+function calculateImc(height, weight){
+    return weight/(height/100)**2
+}
+
+function calculateClass(imc){
+    if(imc < 18.5) return 'peso bajo'
+    if(imc < 25) return 'normal'
+    if(imc < 30) return 'sobrepeso'
+    return 'obesidad'
+}
